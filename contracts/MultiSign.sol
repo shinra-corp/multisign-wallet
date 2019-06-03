a542801f2f22ea9f53767b0c12ddbd33254e86a9
logic <logic@mothership.local>
2019-06-03 17:25:14 +0100

    initial code

---
+++ contracts/MultiSign.sol
@@ -0,0 +1,186 @@
+pragma solidity ^0.5.8;
+
+contract MultiSign {
+
+    event Deposit(address indexed from, uint256 value);
+    event Execution(
+        address indexed from, 
+        uint256 value, 
+        address indexed to, 
+        uint256 gasLimit, 
+        bytes data, 
+        bool success, 
+        bytes result
+    );
+    event Shutdown(address indexed from, bool shutdown);
+
+    //Query Owners of this wallet
+    address[] public owners;
+    //Minimal number of signatures to make a execution call
+    uint256 public threshold;
+    //To avoid replay from diferent networks 
+    uint256 public chainId;
+    //To avoid replay of the same transaction
+    uint256 public nonce;
+
+    bool internal _lock;
+    bool internal _shutdown;
+
+    string constant HEADER = "\x19Ethereum Signed Message:\n32";
+
+    mapping(address => bool) internal _isOwner;
+    mapping(uint256 => bool) internal _usedNonce;
+
+    constructor(
+        address[] memory _owners, 
+        uint256 _threshold, 
+        uint256 _chainId
+    ) 
+    public 
+    {
+        require(_threshold > 0, 'Threshold not valid');
+        require(_owners.length >= _threshold, 'invalid peers addresses');
+        
+        threshold = _threshold;
+        owners = _owners;
+        chainId = _chainId;
+        
+        for(uint256 i = 0; i < owners.length; i++) {
+           require(!_isOwner[owners[i]] && owners[i] != address(0x0)); 
+            _isOwner[owners[i]] = true;
+
+        }
+    }
+
+    function getOwners() external view returns(address[] memory) {
+        return owners; 
+    }
+    
+    function() external payable {
+        if(msg.value > 0) {
+            emit Deposit(msg.sender, msg.value);
+        }
+    }
+    
+    function execute(
+        bytes calldata _signatures,
+        uint256 _nonce,
+        uint256 _amount, 
+        address payable _destination, 
+        uint256 _gasLimit, 
+        bytes calldata _data
+    ) 
+    external 
+    shutdown(_destination)
+    {
+        require(!_usedNonce[nonce], 'nonce used');
+        
+        bytes32 _hash = keccak256(
+            abi.encodePacked
+                (HEADER, 
+                 keccak256(abi.encodePacked(_nonce, _amount, _destination, _gasLimit, _data))
+                )
+        );
+
+        address _testAddr = address(0x0);
+        address _lastAddr = address(0x0);
+        uint256 i;
+       
+        while(i < threshold) {
+            _testAddr = recovery(_hash, _signatures, i);
+            require(_lastAddr < _testAddr, 'signature order should be ASC');
+            require(_isOwner[_testAddr], 'not peer');            
+            _lastAddr = _testAddr;
+            i++;
+        }
+
+        require(i == threshold, "threshold not meet");
+       
+        bool success;
+        bytes memory result;
+        
+        (success, result) = _execute(_amount, _destination, _gasLimit, _data);
+        require(success, 'executing external call');
+        nonce = nonce + 1;
+
+        //Emit execution event
+        emit Execution(msg.sender, _amount, _destination, _gasLimit, _data, success, result);
+        //return (success, result);
+    }
+
+
+    function setShutdown(bool _isShutdown) external isOwner {
+        _shutdown = _isShutdown;
+        emit Shutdown(msg.sender, _shutdown);
+    }
+
+
+    function getShutdown() external view returns(bool) {
+        return _shutdown;
+    }       
+
+
+    function recovery(bytes32 _hash, bytes memory _signatures, uint256 _index) internal pure returns(address) {
+        bytes32 r;
+        bytes32 s;
+        uint8 v;
+
+        (r,s,v) = _slice(_signatures, _index);
+        return ecrecover(_hash, v, r, s);
+    }
+
+
+    //Slice the bytes to signatures;
+    function _slice(bytes memory _signatures, uint256 _index) internal pure returns(bytes32 r, bytes32 s, uint8 v) {
+        
+        assembly {
+            let _offset := mul(_index, 65) 
+            r := mload(add(_signatures, add(32, _offset)))
+            s := mload(add(_signatures, add(64, _offset)))
+            v := and(mload(add(_signatures, add(65, _offset))), 0xff)
+        }
+
+        if(v < 27) {
+            v += 27;
+        }
+
+        require(v == 27 || v == 28, "signature invalid, not v==27 || v==28");
+
+    }
+
+
+    function _execute(
+        uint256 _amount, 
+        address payable _destination, 
+        uint256 _gasLimit, 
+        bytes memory _data
+    ) 
+    internal
+    lock
+    returns(bool, bytes memory) 
+    {
+        return _destination.call.gas(_gasLimit).value(_amount)(_data);
+    }
+
+
+    modifier isOwner {
+        require(_isOwner[msg.sender], 'not owner');
+        _;
+    }
+
+
+    modifier lock {
+        require(!_lock, 'resource in use');
+        
+        _lock = true;
+        _;
+        _lock = false;
+    }
+
+    modifier shutdown(address _dest) {
+        if(_shutdown) {
+            require(_isOwner[_dest]);
+        } 
+        _;
+    }
+}
---
+++ test/multiSigTests.js
@@ -0,0 +1,215 @@
+/*
+ *Mnemonic (Ganache): citizen home donor pepper arrow group gallery liberty neutral outer return alcohol
+ */
+
+const { 
+    BN, 
+    constants, 
+    balance,
+    expectEvent, 
+    expectRevert
+} = require('openzeppelin-test-helpers');
+
+chai = require('chai')
+, expect = chai.expect
+, should = chai.should();
+
+const Wallet = artifacts.require("MultiSign");
+
+var account1 = {
+    addr: "0x361caDAD984c5e5A4e695A001793364D4A153ec5",
+    pk: "0x358739417a53b506ef2c3cd2ed5f5d3b7a09f9be8fdce6bc1321cfbbb8cd0def", 
+    signature: "" 
+}
+var account2 = {
+    addr: "0x70AD3Be8a44Cc542b3DBeFC31fb3C39f7b2bffe6",
+    pk: "0x444fc495516eb70e6d400ed4da762028d48c3b8d2ce1acf82b9aa15feaed62f9",
+    signature: "" 
+}
+var account3 = {
+    addr:"0x0E2f84C3CbF0A5daa47d0dFa8Ab51Fd5BA195CA0",
+    pk:"0x4c064f3c5a50e682b36f9aea8dd85b4f60fc1884c0848d63552910caf8f2205c", 
+    signature: ""
+}
+
+
+contract("Deployment Tests", async accounts => {
+    
+    let instance;
+    let owners = new Array(account1.addr, account2.addr, account3.addr);
+    let threshold = 3;
+    let chainId = 5777;
+
+    beforeEach("deploy a new instance", async () => {
+        instance = await Wallet.new(owners, threshold, chainId);
+    });
+
+    it("should start with a zero nonce", async () => {
+        assert.equal(await instance.nonce(), 0, "should be a zero nonce");
+    });
+
+    it("should be in the right network", async () => {
+        assert.equal(await instance.chainId(), chainId, "should have the right network id");
+    });
+
+    it("should have the right owners", async () => {
+        let _owners = await instance.getOwners();
+        expect(_owners).to.have.members(owners);
+    });
+
+});
+
+
+
+contract("MultiSignature deployment negative tests", async accounts => {
+    let instance;
+    let owners = new Array(accounts[0], accounts[1], accounts[2]);
+    let threshold = 2;
+    let chainId = 5777;
+
+    it("should not continue without a treshold", async () => {
+        expectRevert(Wallet.new(owners, 0, chainId), "Threshold not valid");
+    });
+
+    it("should not continue without a proper more owners than threshold", async () => {
+        let minOwners = new Array(accounts[0]);
+        expectRevert(Wallet.new(owners, 4, chainId), "invalid peers addresses");
+        expectRevert(Wallet.new(minOwners, threshold, chainId), "invalid peers addresses");
+    });
+});
+
+
+contract("MultiSignature Deposit Ether", async accounts => {
+    let instance;
+    let owners = new Array(accounts[0], accounts[1], accounts[2]);
+    let threshold = 2;
+    let chainId = 5777;
+
+    let amount = web3.utils.toWei("1", "ether");
+
+    beforeEach("deploy a new instance", async () => {
+        instance = await Wallet.new(owners, threshold, chainId); 
+    });
+
+    it("should have the correct balance", async () => {
+        
+        const balanceTracker = await balance.tracker(instance.address);
+        await instance.sendTransaction({value: amount});
+        (await balanceTracker.delta()).should.be.bignumber.equal(amount);
+    });
+
+    it("should emit Deposit event", async () => {
+       
+        var {logs} = await instance.sendTransaction({value:amount}); 
+        expectEvent.inLogs(logs, "Deposit", {from: accounts[0], value: amount});
+    });
+});
+contract("Shutdown change of state", async accounts => {
+    let instance;
+    let owners = new Array(accounts[0], accounts[1], accounts[2]);
+    let threshold = 2;
+    let chainId = 5777;
+
+    beforeEach("deploy a new instance", async () => {
+        instance = await Wallet.new(owners, threshold, chainId);
+    });
+
+    it("should change shutdown state", async () => {
+        let state = await instance.getShutdown();
+        await instance.setShutdown(true, {from: accounts[0]});
+
+        assert.notEqual(state, await instance.getShutdown(), "Shutdown state do not change");
+    }); 
+
+    it("should emit shutdown event", async () => {
+        var {logs} = await instance.setShutdown(true, {from: accounts[0]});
+        expectEvent.inLogs(logs, "Shutdown", {from: accounts[0], shutdown : true})
+    });
+
+});
+
+contract("Shutdown change of state negative test", async accounts => {
+
+    let instance;
+    let owners = new Array(accounts[0], accounts[1], accounts[2]);
+    let threshold = 2;
+    let chainId = 5777;
+
+    beforeEach("deploy a new instance", async () => {
+        instance = await Wallet.new(owners, threshold, chainId);
+    });
+
+    it("Should not change if not owner", async () => {
+        await expectRevert(instance.setShutdown(false, {from:accounts[7]}), "not owner");
+    });
+
+});
+
+
+contract("Signatures Tests", async accounts => {
+
+    let instance;
+    let owners = new Array(account1.addr, account2.addr, account3.addr);
+    let threshold = 3;
+    let chainId = 5777;
+    let nonce = 1;
+    let gasLimit = 1;
+    let data = "0x";
+
+    beforeEach("deploy a new instance", async () => {
+        instance = await Wallet.new(owners, threshold, chainId);
+        
+        let amount =  web3.utils.toWei("1", "ether");
+        await instance.sendTransaction({value: amount});
+    });
+
+    it("validate signature", async () => {
+        let amount = web3.utils.toWei("1", "ether");
+        let destination = accounts[5];
+        let prefix = "\x19Ethereum Signed Message:\n32";
+        let hash = await web3.utils.soliditySha3(nonce, amount, destination, gasLimit, data);
+
+        account1.signature = await web3.eth.accounts.sign(hash, account1.pk);
+        account2.signature = await web3.eth.accounts.sign(hash, account2.pk);
+        account3.signature = await web3.eth.accounts.sign(hash, account3.pk);
+
+        let _objs = new Array(
+            account1,
+            account2,
+            account3
+        )
+
+        let finalSign = _objs.sort((a,b) => a.addr > b.addr);
+        let signatures = finalSign.map(a => a.signature.signature.replace('0x', '')).join('');
+        await instance.execute('0x' + signatures, nonce, amount, destination, gasLimit, data);
+    });
+});
+
+/*
+contract("Signature negative Tests", async accounts => {
+    
+    let instance;
+    let owners = new Array(account1.addr, account2.addr, account3.addr);
+    let threshold = 3;
+    let chainId = 5777;
+    let nonce = 1;
+    let gasLimit = 1;
+    let data = "0x";
+
+    beforeEach("deploy a new instance", async () => {
+        instance = await Wallet.new(owners, threshold, chainId);
+        let amount =  web3.utils.toWei("1", "ether");
+        await instance.sendTransaction({value: amount});
+    });
+    //tests : wrong parameters
+    //alter signatures
+    //omit signature
+    //less than threshold
+    //same singnature 3x
+    //same signature 2x
+    //change order 
+    //feed another signature of a not peers address
+    //it("should emit a revert when mismatch parameters to hash", async => {
+    //});
+});
+*/
